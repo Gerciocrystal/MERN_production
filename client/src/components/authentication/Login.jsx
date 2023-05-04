7c885d88f6d0f36bb7dbfc02e9c1a773a0e0cacd
Jercio Narciso Bande <90478299+Gerciocrystal@users.noreply.github.com>
2023-05-04 10:12:43 +0200

    problema de cors

--- client/src/components/authentication/Login.jsx
+++ client/src/components/authentication/Login.jsx
@@ -42,7 +42,7 @@ const Login = () => {
         },
       };
       const { data } = await axios.post(
-        "https://mern-chat-production.onrender.com/user/login",
+        "/api/user/login",
         {
           email,
           password,
