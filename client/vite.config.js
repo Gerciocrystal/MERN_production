abefb76bd497bd76283664c7f7d8c4c88b0e36f8
Jercio Narciso Bande <90478299+Gerciocrystal@users.noreply.github.com>
2023-05-04 10:14:30 +0200

    vite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "https://mern-chat-production.onrender.com/",
+      "/api": "https://mern-chat-production.onrender.com",
     },
   },
   plugins: [react()],
