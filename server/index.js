8dfc45d4f88b101941005b8e76bb52bae556654b
Jercio Narciso Bande <90478299+Gerciocrystal@users.noreply.github.com>
2023-05-06 15:37:03 +0200

    clearing the production

--- server/index.js
+++ server/index.js
@@ -13,24 +13,10 @@ connectDb();
 app.use(express.json());
 app.use(cors());
 app.use("/api", Routes);
-// ------------------------------------Production---------
-const __dirname1 = path.resolve();
-console.log(__dirname1);
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/../client/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname1, "..", "client", "dist", "index.html")
-    );
-  });
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running");
-  });
-}
 
-// ------------------------------------Production---------
+
+
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`.yellow.bold);
