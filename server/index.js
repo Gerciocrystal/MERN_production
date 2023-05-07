c097fa42cdfada09cdd506b2d41d8db3c2528fff
Jercio Narciso Bande <90478299+Gerciocrystal@users.noreply.github.com>
2023-05-07 12:33:09 +0200

    add static dist

--- server/index.js
+++ server/index.js
@@ -14,8 +14,10 @@ app.use(express.json());
 app.use(cors());
 app.use("/api", Routes);
 
+// ------------------------------------Production---------
+app.use(express.static("dist"));
 
-
+// ------------------------------------Production---------
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => {
