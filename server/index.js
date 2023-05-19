f3593ad48f5be6d7d8e42f4a9047364396e2e7a6
Jercio Narciso Bande <90478299+Gerciocrystal@users.noreply.github.com>
2023-05-19 14:20:08 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,8 +15,20 @@ app.use(cors());
 app.use("/api", Routes);
 
 // ------------------------------------Production---------
-app.use(express.static("dist"));
+//app.use(express.static("dist"));
+const __dirname1 = path.resolve();
+console.log(__dirname1);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "./dist")));
 
+  app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "dist", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running");
+  });
+}
 // ------------------------------------Production---------
 
 const PORT = process.env.PORT || 5000;
